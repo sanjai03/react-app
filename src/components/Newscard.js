082e83a1eb020b43c894f9e7df0394699a0411cf
unknown <iveejnas45@gmail.com>
2021-01-27 18:29:37 +0530

    allign the post content

--- src/components/Newscard.js
+++ src/components/Newscard.js
@@ -1,11 +1,11 @@
 function Newscard(props){
     return(
         <div class="p-3 card">
-        <img src={props.img} width="100%" class="px-1 py-1" />
+        
     <h4 class=" product-txt px-1">{props.name}</h4>
         <p class=" px-1 text-primary">{props.desc}
         </p>
-        <p class="row"><span class="col-6 text-primary">5 april</span><span class="col-6 text-primary">5 comments</span></p>
+        <p class="row"><span class="col-6 text-primary">view</span><span class="col-6 text-primary">comments</span></p>
       </div>
     )
 }
