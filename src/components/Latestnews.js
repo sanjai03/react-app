96706490db7657c1b292d9ea4d9b3057d5dc0667
unknown <iveejnas45@gmail.com>
2021-01-27 18:28:52 +0530

    assign the array value using map function

--- src/components/Latestnews.js
+++ src/components/Latestnews.js
@@ -1,6 +1,6 @@
 import Newscard from './Newscard'
 import image from '../image/nutrion.jpg'
-function Latestnews(){
+function Latestnews(props){
     const clubName="Nutrient Club"
     const describtion = "Concurrently, plant sources of ephedrine were removed from Herbalife products in 2002 after several U.S. states banned supplements containing such herbs"
     return(
@@ -9,15 +9,16 @@ function Latestnews(){
         <div className ="col-10 row">
         <div class="col-10"><h4 class="text-success">Recent News</h4><h2 class="align-left">Our Latest News</h2></div>
         <div class="col-2"><button class="button1">view more</button></div>
-        <div class="col-4">
-        <Newscard img={image} name={clubName} desc={describtion} />
-        </div>
-        <div class="col-4">
-        <Newscard img={image} name={clubName} desc={describtion} />
-        </div>
-        <div class="col-4">
-        <Newscard img={image} name={clubName} desc={describtion} />
-        </div>
+        {
+            props.posts.map((post)=>{
+                return(
+                    <div class="col-4">
+                        <Newscard name={post.title} desc={post.body} />
+                    </div>
+                )
+            })
+        }
+        
         </div>
 </div>
     )
