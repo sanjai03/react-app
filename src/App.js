12688ab776667e7f2d310c95c7efb168b5d03f50
unknown <iveejnas45@gmail.com>
2021-01-28 11:25:16 +0530

    implement router in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './view/Home';
-
+import Post from './view/Post';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+            <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/post/:id">
+          <Post />
+        </Route>
+        
+      </Switch>
+    </Router>
+      
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
